70309ff331de12523da3880a47952a078b77fd09
userJoshu <marcelljis23@gmail.com>
2024-07-21 21:36:54 +0700

    popupKategori

--- Monye-T_Project/routes/web.php
+++ Monye-T_Project/routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\CategoryController;
 use App\Http\Controllers\DompetController;
 use App\Http\Controllers\TransactionController;
 use App\Http\Controllers\PencatatanController;
@@ -65,3 +66,6 @@ Route::get('/pencatatan', [PencatatanController::class, 'index'])->name('pencata
 Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index');
 
 
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
+Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
+Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
