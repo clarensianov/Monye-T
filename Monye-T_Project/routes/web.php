6e3bf69485e93130db31464bf2e35fd70bb789ab
clarensianov <127484756+clarensianov@users.noreply.github.com>
2024-07-26 08:24:11 +0700

    Web Change

--- Monye-T_Project/routes/web.php
+++ Monye-T_Project/routes/web.php
@@ -9,7 +9,6 @@ use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
-use App\Models\Kategori;
 use App\Models\Category;
 
 /*
@@ -48,10 +47,12 @@ Route::put('/inputsandi/{id}', [UserController::class, 'inputsandi'])->name('cha
 
 
 Route::get('/dashboard', function(){
-    $categories = Category::all();
-    return view('dashboard' , ['categories' => $categories]);
-    return view('dashboard');
+    // Take all user's dompet (function found in user model)
+    // $dompets = Auth::user()->dompets;
+    // dd($dompets);
 
+    // return view('dashboard', compact('dompets'));
+    return view('dashboard');
 })->name('dashboard')->middleware('auth');
 
 Route::post('/inputDompet', [DompetController::class, 'inputDompet'])->name('input_dompet');
@@ -69,12 +70,14 @@ Route::get('/profile', [ProfileController::class, 'index'])->name('profile.index
 Route::post('/profile/update', [ProfileController::class, 'updateProfile'])->name('profile.update');
 
 
+Route::get('/categories', [CategoryController::class, 'index'])->name('categories.index');
 Route::post('/categories', [CategoryController::class, 'store'])->name('categories.store');
 Route::put('/categories/{category}', [CategoryController::class, 'update'])->name('categories.update');
 
 Route::get('/anggaran', [AnggaranController::class, 'index'])->name('anggaran')->middleware('auth');
 Route::get('/exanggaran', [AnggaranController::class, 'nonIndex'])->name('anggaran.non')->middleware('auth');
 Route::post('/anggaran', [AnggaranController::class, 'create'])->name('anggaran.create');
+
 Route::get('/AnggaranAktif' , function(){
     $categories = Category::all();
     return view('AnggaranAktif' , ['categories' => $categories]);
